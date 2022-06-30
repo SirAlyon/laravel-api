9ed26d248f7470bd8cf2659254c4b1f86e1f197f
SirAlyon <alessiosada04@gmail.com>
2022-06-30 15:15:24 +0200

    add live

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,9 +18,9 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import router from './router';
 
 import App from './views/App';
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,5 +29,6 @@ import App from './views/App';
 
 const app = new Vue({
     el: '#root',
-    render: h => h(App)
+    render: h => h(App),
+        router
 });
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,184 +1,59 @@
 <template>
   <section>
-    <h1>Work in progress ... :D</h1>
+    <!-- use router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
 
-    <div class="container-fluid px-3">
-      <div class="row">
-        <div class="col-10">
-          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-            <div class="col" v-for="post in postsResponse.data" :key="post.id">
-              <div class="product card">
-                <img :src="'storage/' + post.cover_image" :alt="post.title" />
-                <div class="card-body">
-                  <h3>{{ post.title }}</h3>
-                  <p>{{ post.content }}</p>
-                </div>
-                <div class="card-footer">
-                  <div class="row">
-                    <div class="col">
-                      <div class="author" v-if="post.user">
-                        <h4>Autor</h4>
-                        {{ post.user.name }}
-                      </div>
-                    </div>
-                  </div>
-                  <span v-if="post.category">
-                    <strong>Category:</strong>
-                    {{ post.category.name }}
-                  </span>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-                  <div class="post_tags" v-if="post.tags.length > 0">
-                    <strong>Tags:</strong>
-                    <span v-for="(tag, index) in post.tags" :key="index">
-                      {{ formatTags(index, tag, post.tags) }}</span
-                    >
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-2">
-          <div class="col-12">
-            <div class="categories">
-              <strong class="fs-4">Categories:</strong>
-              <ul class="list-unstyled">
-                <li v-for="category in categories" :key="category.id">
-                  - {{ category.name }}
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-12">
-            <div class="tags">
-              <strong class="fs-4">Tags:</strong>
-              <ul class="list-unstyled">
-                <li v-for="tag in tags" :key="tag.id"># {{ tag.name }}</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active" v-for="(item, index) in menu_items" :key="index">
+             <router-link class="nav-link" :to="{name: item.route_name}">{{item.route_text}}</router-link>
+            
+          </li>
+
+        </ul>
       </div>
-    </div>
-
-    <nav aria-label="Page navigation example ">
-      <ul class="pagination justify-content-center mt-5">
-        <li class="page-item" v-if="postsResponse.current_page > 1">
-          <a
-            class="page-link"
-            href="#"
-            @click.prevent="getAllPost(postsResponse.current_page - 1)"
-            >Previous</a
-          >
-        </li>
-        <li
-          :class="{
-            'page-item': true,
-            active: page == postsResponse.current_page,
-          }"
-          v-for="(page, index) in postsResponse.last_page"
-          :key="index"
-        >
-          <a class="page-link" href="#" @click.prevent="getAllPost(page)">{{
-            page
-          }}</a>
-        </li>
-        <li
-          class="page-item"
-          v-if="postsResponse.current_page < postsResponse.last_page"
-        >
-          <a
-            class="page-link"
-            href="#"
-            @click.prevent="getAllPost(postsResponse.current_page + 1)"
-            >Next</a
-          >
-        </li>
-      </ul>
     </nav>
+    <router-view></router-view>
   </section>
+
+  
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
-  mounted() {
-    console.log("Mounted Successfully");
-    this.getAllPost(1);
-    this.getAllCategories();
-    this.getAllTags();
-  },
-  methods: {
-    getAllPost(postPage) {
-      console.log(postPage);
-      axios
-        .get("/api/posts", {
-          params: {
-            page: postPage,
-          },
-        })
-        .then((response) => {
-          console.log(response);
-          this.posts = response.data.data;
-          this.postsResponse = response.data;
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    },
-    getAllCategories() {
-      axios
-        .get("/api/categories")
-        .then((response) => {
-          console.log(response);
-          this.categories = response.data;
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    },
-    getAllTags() {
-      axios
-        .get("/api/tags")
-        .then((response) => {
-          console.log(response);
-          this.tags = response.data;
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    },
-    formatTags(index, tag, tags) {
-      console.log(tags.length, index);
-      if (index == tags.length - 1) {
-        return tag.name + ".";
-      } else {
-        return tag.name + ",";
-      }
-    },
-  },
   data() {
     return {
-      posts: "",
-      postsResponse: "",
-      categories: "",
-      tags: "",
+      menu_items: [
+        {
+          route_name: "home",
+          route_text: "Home",
+        },
+        {
+          route_name: "about",
+          route_text: "About",
+        },
+        {
+          route_name: "posts",
+          route_text: "Posts",
+        },
+      ],
     };
   },
 };
 </script>
 
-<style scoped lang="scss">
-h1 {
-  color: red;
-}
-
-.categories,
-.tags{
-  padding: 1rem;
-  margin-top: 1rem;
-  background-color: rgba(211, 211, 211, 0.621);
-  border-radius: 0.5rem;
-}
-</style>
